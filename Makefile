a80937abd150c3fb8edddde2bd3c8925245ab141
David Rees <davidrees191@gmail.com>
2022-05-18 16:33:59 -0700

    starting gui

---
+++ Makefile
@@ -0,0 +1,7 @@
+all: compile link
+
+compile:
+	g++ -I src/include -c wordle.cpp
+
+link:
+	g++ wordle.o -o wordle -L src/lib -l sfml-graphics -l sfml-window -l sfml-system
--- wordle.cpp
+++ wordle.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <array>
 #include "Wordle_Clone.h"
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
@@ -8,10 +9,28 @@ using namespace std;
 int main(){
 
     // Creates a game object
-    Wordle_Clone game{}; 
+    //Wordle_Clone game{}; 
 
     // Activates the game
-    game.start_game(game.get_word(), game.get_word_list());
+    //game.start_game(game.get_word(), game.get_word_list());
+
+    sf::RenderWindow window(sf::VideoMode(600, 600), "Wordle Clone");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Red);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
     
     return 0;
 
